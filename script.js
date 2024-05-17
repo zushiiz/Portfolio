5a67f9918bb456774fc9f28b5e05b6dadc4c298e
zushii <zxgamingh@gmail.com>
2024-05-17 12:00:43 +0200

    shitty darkmode

--- script.js
+++ script.js
@@ -24,9 +24,7 @@ function scrollContact() {
   element.scrollIntoView({ behavior: "smooth", block: "start" });
 }
 
-
 let slideIndex = 0;
-
 showSlide();
 
 function showSlide() {
@@ -46,3 +44,16 @@ function showSlide() {
   }
   dots[slideIndex-1].className += " active";
 }
+
+const switchTheme = () => {
+  const rootElem = document.documentElement
+  let dataTheme = rootElem.getAttribute("data-theme"),
+      newTheme
+
+  newTheme = (dataTheme === "light") ? "dark" : "light"
+
+  rootElem.setAttribute("data-theme", newTheme)
+
+  localStorage.setItem("theme", newTheme)
+}
+document.querySelector('#theme-switcher').addEventListener('click', switchTheme)
